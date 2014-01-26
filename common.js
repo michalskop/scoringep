2822028f62be6471fefefc9d615d459a46f4b5b8
Joachim Gola <joachim@viergleicheins.de>
2014-01-26 11:30:52 +0100

    extracted JS, css

---
+++ common.js
@@ -0,0 +1,49 @@
+  //get url params
+  
+  function getParameter(paramName) {
+      var searchString = window.location.search.substring(1),
+          i, val, params = searchString.split("&");
+    
+      for (i=0;i<params.length;i++) {
+        val = params[i].split("=");
+        if (val[0] == paramName) {
+          return unescape(val[1]);
+        }
+      }
+      return null;
+  }
+
+  //gets the color and description based on the score
+  function score2cd (score,limits) {
+    out = new Object;
+    out.color = 'gray';
+    out.text = ' is a great guy';
+    for (i = 0; i < limits.length; i++) {
+      if (score <= limits[i].upper) return limits[i];
+    }
+    return out;
+  }
+  
+  //function calculate score
+  function scoreit(mepvotes,topics ) {
+    //reorder campaign data for easier access
+    votings = new Object;
+    $.each(topics, function (i0, v0) {
+      $.each(v0.votings, function (i, v) {
+        votings[v.v_dbid] = v;
+      });
+    });
+    wsum = 0;
+    vsum = 0;
+    $.each(mepvotes, function( index, value ) {
+      if ((votings[value.v_dbid] != 'undefined') && (value.exists)) {
+        wsum += votings[value.v_dbid].v_weight;
+        vsum += votings[value.v_dbid].v_weight*parseInt(value.vote)*parseInt(votings[value.v_dbid].v_recommendation);
+      }
+    });
+    if (wsum != 0)
+      out = Math.round((vsum/wsum + 1)/2 * 100);
+    else
+      out = 50; 
+    return out;
+  }
