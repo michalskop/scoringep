e73c9f3cf7592d889efa6976d6f357244bad47ca
Joachim Gola <joachim@viergleicheins.de>
2014-01-26 18:12:23 +0100

    last changes before presentation

---
+++ score.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Scoring EP</title>
+	<link rel="shortcut icon" href="http://demos.jquerymobile.com/favicon.ico">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="http://demos.jquerymobile.com/1.4.0/css/themes/default/jquery.mobile-1.4.0.min.css">
+	<link rel="stylesheet" href="http://demos.jquerymobile.com/1.4.0/_assets/css/jqm-demos.css">
+	<link rel="stylesheet" href="styles.css">
+	<script src="http://demos.jquerymobile.com/1.4.0/js/jquery.js"></script>
+	<script src="http://demos.jquerymobile.com/1.4.0/_assets/js/index.js"></script>
+	<script src="http://demos.jquerymobile.com/1.4.0/js/jquery.mobile-1.4.0.min.js"></script>
+	<script src="common.js"></script>	
+	
+</head>
+<body>
+<div data-role="page" class="jqm-demos" data-quicklinks="false" id="mepPage">
+	<div data-role="header" class="jqm-header">
+		<h2><img alt="Scoring EP" id="logo" src="http://www.sandbag.org.uk/site_media/images/sandbag_logo_transparent.png"></h2>
+		<p>Version 0</p>
+	</div><!-- /header -->
+	
+	<div role="main" class="ui-content jqm-content">
+        <h1 class="starthead"></h1>
+		<p> We examined MEPs voting records across 11 key votes over the last 5 years. The votes related to various different aspects of climate change policy. We gave each vote a weighting of importance, then gave the MEP that score for that vote if they voted in the direction most favourable for ambitious action on climate change. By averaging the scores across all the votes per MEP, we obtained the final rating.
+		</p>
+	    <div>
+            <a class="ui-btn ui-icon-arrow-r ui-button-icon-right" href="list.php" data-ajax="false">Find your MEP</a>
+	    </div>
+    </div>
+</div>
+</body>
+</hmtl>
+
