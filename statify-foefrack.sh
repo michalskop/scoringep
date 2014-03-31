6c1eb450b0c1eaedd251ea640df887675f0b6b65
Joachim Gola <joachim@viergleicheins.de>
2014-03-31 15:14:53 +0200

    new foe stuff

---
+++ statify-foefrack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in $(seq 1000)
+do
+  curl "http://localhost:8888/euparlameter/ep11/json_abg_detail.php?id=$i&v_dbids=3358,3361,4787,4788,4792,5689,5747,5748" -o "static/mep$i.json"
+done
