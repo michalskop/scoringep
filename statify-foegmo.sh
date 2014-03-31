2842b210449196ca84f0644516fa0c5c88127699
Joachim Gola <joachim@viergleicheins.de>
2014-03-31 16:06:30 +0200

    data for foe (tracking and gmo)

--- statify-foegmo.sh
+++ statify-foegmo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(seq 1000)
 do
-  curl "http://localhost:8888/euparlameter/ep11/json_abg_detail.php?id=$i&v_dbids=2039,2041,2043,5428" -o "static/mep$i.json"
+  curl "http://localhost:8888/euparlameter/ep11/json_abg_detail.php?id=$i&v_dbids=2039,2041,2043,5428" -o "static_foegmo/mep$i.json"
 done
