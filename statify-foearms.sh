b33bce34bb35b24f575a46933f1ffaa05970edd5
Joachim Gola <joachim@viergleicheins.de>
2014-05-05 18:50:57 +0200

    fix:missing comma

--- statify-foearms.sh
+++ statify-foearms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(seq 1000)
 do
-  curl "http://localhost:8888/euparlameter/ep11/json_abg_detail.php?id=$i&v_dbids=5249,4462,5216,5217,4662,44475227,5243,4458,4657,4661" -o "static_foearms/mep$i.json"
+  curl "http://localhost:8888/euparlameter/ep11/json_abg_detail.php?id=$i&v_dbids=5249,4462,5216,5217,4662,4447,5227,5243,4458,4657,4661" -o "foe/static_foearms/mep$i.json"
 done
